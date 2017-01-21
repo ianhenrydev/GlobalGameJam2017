221cac73598fe07f09db01dc28626e91c782c13f
Ian Henry <ian@ianhenry.me>
2017-01-20 23:23:46 -0500

    added start menu and start of player selection screen

---
+++ RaftGame/Assets/MainMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour {
+
+	public GameObject mainCanvas;
+	public GameObject playerSelectCanvas;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void PlayOnClick(){
+		mainCanvas.SetActive (false);
+		playerSelectCanvas.SetActive (true);
+	}
+}
---
+++ RaftGame/Assets/PlayerSelectPanelController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSelectPanelController : MonoBehaviour {
+
+	public Text promptText;
+	private bool playerJoined = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("space")) {
+			playerJoined = !playerJoined;
+			if (playerJoined)
+				promptText.text = "Press space to join...";
+			else
+				promptText.text = "Player joined";
+		}
+	}
+}
