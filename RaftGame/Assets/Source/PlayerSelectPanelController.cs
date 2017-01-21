341c2203a9c980d07c4fe43538a83be3f9fef735
Ian Henry <ian@ianhenry.me>
2017-01-21 15:06:30 -0500

    main menu working now

---
+++ RaftGame/Assets/Source/PlayerSelectPanelController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class PlayerSelectPanelController : MonoBehaviour {
+
+	public GameObject joinedLayout;
+	public GameObject promptText;
+	public Text teamText;
+	public int playerNum;
+
+	private bool playerJoined = false;
+	private bool team1 = true;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown("Submit" + playerNum.ToString())) {
+			if (!playerJoined) {
+				promptText.SetActive (false);
+				joinedLayout.SetActive (true);
+				playerJoined = !playerJoined;
+			}
+		}
+		if (Input.GetButtonDown("Cancel" + playerNum.ToString())) {
+			if (playerJoined) {
+				promptText.SetActive (true);
+				joinedLayout.SetActive (false);
+				playerJoined = !playerJoined;
+			}
+		}
+		if (Input.GetButtonDown("ChangeTeam" + playerNum.ToString())) {
+			if (playerJoined) {
+				switchTeam ();
+			}
+		}
+	}
+
+	public void onClickTeam() {
+		switchTeam ();
+	}
+
+	private void switchTeam() {
+		team1 = !team1;
+		if (team1) {
+			teamText.text = "Team 1";
+		} else {
+			teamText.text = "Team 2";
+		}
+	}
+}
